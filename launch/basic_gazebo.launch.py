e9e69d7ca9f76e4127b48e3eaf23128094da07c3
robo <robo2020@gmail.com>
2022-12-25 10:21:24 +0200

    first commit

---
+++ launch/basic_gazebo.launch.py
@@ -0,0 +1,46 @@
+from launch import LaunchDescription
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import AppendEnvironmentVariable, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+PACKAGE = "ros2_gazebo_tutorial"
+WORLD = "empty.world"
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    pkg = get_package_share_directory(PACKAGE)
+    gazebo_pkg = get_package_share_directory('gazebo_ros')
+    
+    verbose = LaunchConfiguration("verbose")
+    arg_gazebo_verbose = DeclareLaunchArgument("verbose", default_value="true")
+    world = LaunchConfiguration("world")
+    arg_gazebo_world = DeclareLaunchArgument("world", default_value=WORLD)
+
+    resources = [os.path.join(pkg, "worlds")]
+
+    resource_env = AppendEnvironmentVariable(
+        name="GAZEBO_RESOURCE_PATH", value=":".join(resources)
+    )
+
+    models = [os.path.join(pkg, "models")]
+
+    models_env = AppendEnvironmentVariable(
+        name="GAZEBO_MODEL_PATH", value=":".join(models)
+    )
+
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    gazebo_pkg, 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'verbose': verbose, "world": world}.items()
+             )
+
+    ld.add_action(models_env)
+    ld.add_action(resource_env)
+    ld.add_action(arg_gazebo_verbose)
+    ld.add_action(arg_gazebo_world)
+    ld.add_action(gazebo)
+    return ld
